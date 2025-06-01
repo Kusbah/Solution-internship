57db8551c76252167a2b46517d019a160d2177fb
kusbah <abdkusbah2@gmail.com>
2025-06-01 14:44:46 +0300

    complete Task 13 code splitting

---
+++ code-splitting/src/App.js
@@ -0,0 +1,24 @@
+
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">US</Link>
+      </nav>
+      <Suspense fallback={<div>Loding ...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+}
+
+export default App;
