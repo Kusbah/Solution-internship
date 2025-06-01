3ba2bfd5162a6fe361342f32d67a51c764d2b6cf
kusbah <abdkusbah2@gmail.com>
2025-06-01 16:41:06 +0300

    complete Task 14 Create a Custom Hook

--- code-splitting/src/App.js
+++ code-splitting/src/App.js
@@ -2,7 +2,7 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-const Home = lazy(() => import('./pages/Home'));
+// const Home = lazy(() => import('./pages/Home'));
 const About = lazy(() => import('./pages/About'));
 
 function App() {
